acb396c73da4c65512c32cf2bd5c5fe3a72f8049
hyunsoo <dreamh1122@gamil.com>
2018-03-01 20:18:09 +0900

    Commit #1: 환경 세팅 (2018. 03. 01)

---
+++ MeantToBe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeantToBe
+//
+//  Created by hyunsu han on 2018. 3. 1..
+//  Copyright © 2018년 hyunsu han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
